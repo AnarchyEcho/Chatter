3ebdab0dc7da12df62d93afc57ed89f151458a72
kodeandre <andre.kodehode@gmail.com>
2021-11-25 22:59:51 +0100

    Chore: rmdir css, touch global styled components

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,47 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import SocketsProvider from '../context/socket.context'
 
-function MyApp({ Component, pageProps }) {
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: #202020; 
+    color: #f5f5f5;
+    box-sizing: border-box;
+  }
+  a {
+  color: inherit;
+  text-decoration: none;
+  }
+  * {
+  box-sizing: border-box;
+  }
+  /* Scrollbar css */
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #20202000;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #F0CE97;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #F0CE97;
+  }
+`
+
+const theme = {}
+
+export default function MyApp({ Component, pageProps }) {
   return (
   <SocketsProvider>
-    <Component {...pageProps} />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
   </SocketsProvider>
   )
 }
-
-export default MyApp
